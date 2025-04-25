49a4e7c09ef842b0fdc1ed238037d61c09fb9c7e
pedro558 <pedroafonso109@gmail.com>
2025-04-25 12:24:36 -0300

    :sparkles: feat: add Heroi class

--- jogo.py
+++ jogo.py
@@ -1,5 +1,15 @@
+# Personagem: classe pai
+# Heroi: classe filha
+# Inimigo: adversario do usuário
 class Personagem:
-  def __init__(self, nome, vida, nivel):
+  def __init__(self, nome: str, vida: int, nivel: int):
+    try:
+      if not isinstance(nome, str) or not isinstance(vida, int) or not isinstance(nivel, int):
+        raise TypeError("Tipos inválidos: 'nome' deve ser string, 'vida' e 'nivel' devem ser inteiros.")
+    except TypeError as e:
+      print(f"Erro: {e}")
+      raise
+
     self.__nome = nome
     self.__vida = vida
     self.__nivel = nivel
@@ -15,7 +25,32 @@ class Personagem:
   @property
   def nivel(self):
     return self.__nivel
+  
+  def exibir_detalhes(self):
+    return f"\nNome: {self.__nome}\nVida: {self.__vida}\nNível: {self.__nivel}"
+  
+class Heroi(Personagem):
+  def __init__(self, nome: str, vida: int, nivel: int, habilidade: str):
+
+    super().__init__(nome,vida,nivel)
+
+    try:
+      if not isinstance(habilidade, str):
+        raise TypeError("Tipo inválido: 'habilidade' deve ser string.")
+    except TypeError as e:
+      print(f"Erro: {e}")
+      raise
+    
+    self.__habilidade = habilidade
+
+  @property
+  def habilidade(self):
+    return self.__habilidade
+  
+  def exibir_detalhes(self):
+    return f"{super().exibir_detalhes()}\nHabilidade: {self.__habilidade}"
+
 
   
-personagem1 = Personagem("Heroi", 100, 1)
-print(f"Nome: {personagem1.nome}, Vida: {personagem1.vida}, Nivel: {personagem1.nivel}")
+heroi = Heroi("Hercules", 100, 1, "Força")
+print(heroi.exibir_detalhes())
